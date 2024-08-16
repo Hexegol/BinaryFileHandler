41cb7fa2693ba19909b3dde185f3f4e910f866c3
GlosXomegalomane <amrounaden@gmail.com>
2024-08-16 21:56:07 +0200

    Update BinaryFileHandler.h

--- BinaryFileHandler.h
+++ BinaryFileHandler.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <vector>
+#include <fstream>
 
 struct Header
 {
-	char signature[4];
+	char signature[40];
 	uint16_t version;
 	uint32_t records;
 
@@ -11,21 +12,20 @@ struct Header
 struct Data
 {
 	uint32_t id;    
-	std::string name;            
+	char name[50];            
 };
 
 class BinaryFileHandler
 {
 private:
 	std::ofstream wOpenFile(const std::string& fileName);
-	void WriteHeader(const std::string& fileName, Header& header);
-	void wWriteRecords(const std::string& fileName, const std::vector<Data>& records);
+	void wWriteHeader(std::ofstream& file, const Header& header);
+	void wWriteRecords(std::ofstream& file, const std::vector<Data>& records);
 
 	std::ifstream rOpenFile(const std::string& fileName);
 	Header  rReadHeader(std::ifstream& file);
-	std::vector<Data> rReadRecords(std::ifstream& file, uint32_t recordCount);
+	void rReadRecords(std::ifstream& file, std::vector<Data>& records);
 public:
 	void Write(const std::string& fileName, const std::vector<Data>& records);
 	void Read(const std::string& fileName, std::vector<Data>& records);
 };
-
